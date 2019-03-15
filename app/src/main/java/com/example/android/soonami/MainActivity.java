e26a899b0a9adbeab79d5fa870549290a6efbc51
Pherlar <christopherlarmour@gmail.com>
2019-03-15 14:04:53 +1100

    added exception logs

--- app/src/main/java/com/example/android/soonami/MainActivity.java
+++ app/src/main/java/com/example/android/soonami/MainActivity.java
@@ -18,6 +18,7 @@ package com.example.android.soonami;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -45,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
     /** URL to query the USGS dataset for earthquake information */
     private static final String USGS_REQUEST_URL =
-            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2012-01-01&endtime=2012-12-01&minmagnitude=6";
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-12-01&minmagnitude=7";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -112,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
             try {
                 jsonResponse = makeHttpRequest(url);
             } catch (IOException e) {
-                // TODO Handle the IOException
+                Log.e(LOG_TAG,"Problem making the http request");
             }
 
             // Extract relevant fields from the JSON response and create an {@link Event} object
@@ -154,6 +155,13 @@ public class MainActivity extends AppCompatActivity {
          */
         private String makeHttpRequest(URL url) throws IOException {
             String jsonResponse = "";
+
+            //If the URL is null, then return early
+            if (url ==null){
+                return jsonResponse;
+            }
+
+
             HttpURLConnection urlConnection = null;
             InputStream inputStream = null;
             try {
@@ -162,10 +170,21 @@ public class MainActivity extends AppCompatActivity {
                 urlConnection.setReadTimeout(10000 /* milliseconds */);
                 urlConnection.setConnectTimeout(15000 /* milliseconds */);
                 urlConnection.connect();
-                inputStream = urlConnection.getInputStream();
-                jsonResponse = readFromStream(inputStream);
+                //if the request is successful (response code 200)
+                //then read the input string and parse the response
+                if (urlConnection.getResponseCode()==200)
+                {
+                    inputStream = urlConnection.getInputStream();
+                    jsonResponse = readFromStream(inputStream);
+                }
+                else{
+                    Log.e(LOG_TAG, "Error Response code: " + urlConnection.getResponseCode());
+                }
+
+
             } catch (IOException e) {
-                // TODO: Handle the exception
+                // handle the exception
+                Log.i(LOG_TAG, "makeHttpRequest: failed with Response other than 200");
             } finally {
                 if (urlConnection != null) {
                     urlConnection.disconnect();
@@ -201,6 +220,12 @@ public class MainActivity extends AppCompatActivity {
          * about the first earthquake from the input earthquakeJSON string.
          */
         private Event extractFeatureFromJson(String earthquakeJSON) {
+            //if the JSON String is empty or null then return early
+            if (TextUtils.isEmpty(earthquakeJSON))
+            {
+                return null;
+            }
+
             try {
                 JSONObject baseJsonResponse = new JSONObject(earthquakeJSON);
                 JSONArray featureArray = baseJsonResponse.getJSONArray("features");
